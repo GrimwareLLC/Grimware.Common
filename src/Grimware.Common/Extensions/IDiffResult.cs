889201a7869b1a76577ff3ceada37d711e713717
GrimGadget <30664204+GrimGadget@users.noreply.github.com>
2019-06-29 15:04:25 -0400

    Added IEnumerableExtensions + IEnumerable.Diff

---
+++ src/Grimware.Common/Extensions/IDiffResult.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Grimware.Extensions
+{
+    public interface IDiffResult<out T>
+    {
+        IEnumerable<T> Added { get; }
+
+        IEnumerable<T> Modified { get; }
+
+        IEnumerable<T> Removed { get; }
+
+        IEnumerable<T> Unmodified { get; }
+    }
+}

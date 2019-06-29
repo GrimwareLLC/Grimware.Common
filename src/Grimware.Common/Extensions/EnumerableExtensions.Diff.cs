59fde25e4569bfc11266cac56cbeabf51dae8d1f
Grim Gadget <30664204+GrimGadget@users.noreply.github.com>
2019-06-29 15:04:25 -0400

    Added IEnumerableExtensions + IEnumerable.Diff

---
+++ src/Grimware.Common/Extensions/EnumerableExtensions.Diff.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Grimware.Extensions
+{
+    partial class EnumerableExtensions
+    {
+        public static IDiffResult<T> Diff<T, TKey, TData>(
+            this IEnumerable<T> previous,
+            IEnumerable<T> current,
+            Func<T, TKey> keySelector,
+            Func<T, TData> dataSelector
+            )
+        {
+            if (keySelector == null)
+                throw new ArgumentNullException(nameof(keySelector));
+            if (dataSelector == null)
+                throw new ArgumentNullException(nameof(dataSelector));
+
+            var diffContext = new DiffContext<T, TKey>(previous, current, keySelector);
+
+            var modifiedKeys = diffContext.SelectModified(dataSelector).ToArray();
+            var unmodifiedKeys = diffContext.SelectIntersecting().Except(modifiedKeys).ToArray();
+
+            return new DiffResult<T>
+            {
+                Removed = SelectValues(diffContext.Previous, diffContext.SelectRemoved()),
+                Added = SelectValues(diffContext.Current, diffContext.SelectAdded()),
+                Modified = SelectValues(diffContext.Current, modifiedKeys),
+                Unmodified = SelectValues(diffContext.Current, unmodifiedKeys),
+            };
+        }
+
+        public static IEnumerable<T> SelectAdded<T, TKey>(
+            this IEnumerable<T> previous,
+            IEnumerable<T> current,
+            Func<T, TKey> keySelector
+            )
+        {
+            if (keySelector == null)
+                throw new ArgumentNullException(nameof(keySelector));
+
+            var diffContext = new DiffContext<T, TKey>(previous, current, keySelector);
+
+            return SelectValues(diffContext.Current, diffContext.SelectAdded());
+        }
+
+        public static IEnumerable<T> SelectModified<T, TKey, TData>(
+            this IEnumerable<T> previous,
+            IEnumerable<T> current,
+            Func<T, TKey> keySelector,
+            Func<T, TData> dataSelector
+            )
+        {
+            if (keySelector == null)
+                throw new ArgumentNullException(nameof(keySelector));
+            if (dataSelector == null)
+                throw new ArgumentNullException(nameof(dataSelector));
+
+            var diffContext = new DiffContext<T, TKey>(previous, current, keySelector);
+
+            return SelectValues(diffContext.Current, diffContext.SelectModified(dataSelector));
+        }
+
+        public static IEnumerable<T> SelectRemoved<T, TKey>(
+            this IEnumerable<T> previous,
+            IEnumerable<T> current,
+            Func<T, TKey> keySelector
+            )
+        {
+            if (keySelector == null)
+                throw new ArgumentNullException(nameof(keySelector));
+
+            var diffContext = new DiffContext<T, TKey>(previous, current, keySelector);
+
+            return SelectValues(diffContext.Previous, diffContext.SelectRemoved());
+        }
+
+        private static IEnumerable<T> SelectValues<T, TKey>(Dictionary<TKey, T> dictionary, IEnumerable<TKey> keys)
+        {
+            return
+                from kv in dictionary
+                where keys.Contains(kv.Key)
+                select kv.Value;
+        }
+
+        #region Nested types
+
+        private class DiffContext<T, TKey>
+        {
+            #region Constructors & Destructor
+
+            public DiffContext(
+                IEnumerable<T> previous,
+                IEnumerable<T> current,
+                Func<T, TKey> keySelector
+                )
+            {
+                if (keySelector == null)
+                    throw new ArgumentNullException(nameof(keySelector));
+
+                Previous = (previous ?? new T[0]).ToDictionary(keySelector);
+                Current = (current ?? new T[0]).ToDictionary(keySelector);
+            }
+
+            #endregion
+
+            public Dictionary<TKey, T> Current { get; }
+
+            public Dictionary<TKey, T> Previous { get; }
+
+            public IEnumerable<TKey> SelectAdded()
+            {
+                return Current.Keys.Except(Previous.Keys);
+            }
+
+            public IEnumerable<TKey> SelectIntersecting()
+            {
+                return Current.Keys.Intersect(Previous.Keys);
+            }
+
+            public IEnumerable<TKey> SelectModified<TData>(Func<T, TData> dataSelector)
+            {
+                var intersectingValues =
+                    from k in SelectIntersecting()
+                    select new
+                    {
+                        Key = k,
+                        CurrentData = dataSelector(Current[k]),
+                        PreviousData = dataSelector(Previous[k]),
+                    };
+
+                return
+                    from a in intersectingValues
+                    where a.CurrentData != null
+                        && !a.CurrentData.Equals(a.PreviousData)
+                    select a.Key;
+            }
+
+            public IEnumerable<TKey> SelectRemoved()
+            {
+                return Previous.Keys.Except(Current.Keys);
+            }
+        }
+
+        private struct DiffResult<T>
+            : IDiffResult<T>
+        {
+            #region IDiffResult<T> Members
+
+            public IEnumerable<T> Added { get; set; }
+
+            public IEnumerable<T> Modified { get; set; }
+
+            public IEnumerable<T> Removed { get; set; }
+
+            public IEnumerable<T> Unmodified { get; set; }
+
+            #endregion
+        }
+
+        #endregion
+    }
+}
---
+++ src/Grimware.Common/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Grimware.Extensions
+{
+    public static partial class EnumerableExtensions
+    {
+        public static IEnumerable<T> Action<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            if (source == null)
+                yield break;
+
+            foreach (var t in source)
+            {
+                action(t);
+                yield return t;
+            }
+        }
+
+        public static IEnumerable<int> AllIndexesWhere<T>(this IEnumerable<T> source, Predicate<T> predicate)
+        {
+            return
+                source
+                    .Select((t, i) => new { Index = i, IsMatch = predicate(t) })
+                    .Where(a => a.IsMatch)
+                    .Select(a => a.Index);
+        }
+
+        public static IEnumerable<T> Concat<T>(this IEnumerable<T> source, T item)
+        {
+            return source == null ? Enumerable.Repeat(item, 1) : source.Concat(Enumerable.Repeat(item, 1));
+        }
+
+        public static IEnumerable<TResult> Convert<T, TResult>(
+            this IEnumerable<T> input,
+            Converter<T, TResult> converter)
+        {
+            return input.Select(t => converter(t));
+        }
+
+        /// <summary>
+        ///     Returns distinct elements from a sequence by using the specified <paramref name="equalityComparison" />
+        ///     and <paramref name="hashCodeGenerator" /> to compare items.
+        /// </summary>
+        /// <typeparam name="T">
+        ///     The type of the elements of <paramref name="source" />.
+        /// </typeparam>
+        /// <param name="source">The sequence from which to remove duplicate elements.</param>
+        /// <param name="equalityComparison">
+        ///     A <see cref="Func&lt;T, T, Boolean&gt;" /> to evaluate the equality of items from the sequence.
+        /// </param>
+        /// <param name="hashCodeGenerator">
+        ///     A <see cref="Func&lt;T, Int32&gt;" /> to generate hash codes for an item.
+        /// </param>
+        /// <returns>
+        ///     An <see cref="IEnumerable&lt;T&gt;" /> that contains distinct elements from the <paramref name="source" /> sequence.
+        /// </returns>
+        public static IEnumerable<T> Distinct<T>(
+            this IEnumerable<T> source,
+            Func<T, T, bool> equalityComparison,
+            Func<T, int> hashCodeGenerator
+            )
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            if (equalityComparison == null)
+                throw new ArgumentNullException(nameof(equalityComparison));
+            if (hashCodeGenerator == null)
+                throw new ArgumentNullException(nameof(hashCodeGenerator));
+
+            return source.Distinct(EqualityComparer.Create(equalityComparison, hashCodeGenerator));
+        }
+
+        /// <summary>
+        ///     Returns distinct elements from a sequence by finding distinct keys using the specified
+        ///     <paramref name="keySelector" />.
+        /// </summary>
+        /// <typeparam name="T">
+        ///     The type of the elements of <paramref name="source" />.
+        /// </typeparam>
+        /// <typeparam name="TKey">
+        ///     The type of keys created by <paramref name="keySelector" />.
+        /// </typeparam>
+        /// <param name="source">The sequence from which to remove duplicate elements.</param>
+        /// <param name="keySelector">
+        ///     A <see cref="Func&lt;T, K&gt;" /> to create a key from an item.
+        /// </param>
+        /// <returns>
+        ///     An <see cref="IEnumerable&lt;T&gt;" /> that contains distinct elements from the <paramref name="source" /> sequence
+        ///     based on key selection.
+        /// </returns>
+        public static IEnumerable<T> Distinct<T, TKey>(this IEnumerable<T> source, Func<T, TKey> keySelector)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            if (keySelector == null)
+                throw new ArgumentNullException(nameof(keySelector));
+
+            return source.Distinct(EqualityComparer.Create(keySelector));
+        }
+
+        public static IEnumerable<T> Except<T>(this IEnumerable<T> source, T item)
+        {
+            return source.Except(Enumerable.Repeat(item, 1));
+        }
+
+        public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            if (source == null)
+                return;
+            if (action == null)
+                return;
+
+            foreach (var t in source)
+                action(t);
+        }
+
+        public static int? FirstIndexWhere<T>(this IEnumerable<T> source, Predicate<T> predicate)
+        {
+            return
+                source
+                    .Select((t, i) => new { Index = i, IsMatch = predicate(t) })
+                    .FirstOrDefault(a => a.IsMatch)
+                    ?.Index;
+        }
+
+        public static IEnumerable<T> Last<T>(this IEnumerable<T> source, int count)
+        {
+            return source.Reverse().Take(count).Reverse();
+        }
+
+        public static bool None<T>(this IEnumerable<T> source)
+        {
+            return !source.Any();
+        }
+
+        public static bool None<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            return !source.Any(predicate);
+        }
+    }
+}

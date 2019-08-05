4e5ca3d1842633b8201fc3b1c2484d79d57ac6a1
Grim Gadget <0664204+GrimGadget@users.noreply.github.com>
2019-08-04 21:57:22 -0400

    Code Improvements & Unit Tests

--- src/Grimware.Common/Extensions/Int32Extensions.cs
+++ src/Grimware.Common/Extensions/Int32Extensions.cs
@@ -5,14 +5,6 @@ namespace Grimware.Extensions
     public static class Int32Extensions
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static int? CountOnBits(this int? value) =>
-            value != null
-                ? value != 0
-                      ? 1 + CountOnBits(value & (value - 1))
-                      : 0
-                : null;
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool? ToBoolean(this int? source) => source != null ? !0.Equals(source.Value) : (bool?)null;
     }
 }
---
+++ tests/Grimware.Common.UnitTests.Shared/Extensions/EnumExtensionsTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Grimware.Common.UnitTests.Extensions
+{
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public class EnumExtensionsTests
+    {
+        [TestMethod]
+        public void Add()
+        {
+
+        }
+
+        [Flags]
+        private enum TestFlags
+        {
+            None = 0,
+
+            BitOne = 1,
+            BitTwo = 2,
+            BitThree = 4,
+            BitFour = 8,
+            BitFive = 16,
+            BitSix = 32,
+            BitSeven = 64,
+            BitEight = 128,
+
+            All = 0xFF,
+        }
+    }
+}

a829d2c93e1f629f7188e47919cbe91cbd909464
GrimGadget <30664204+GrimGadget@users.noreply.github.com>
2019-08-07 14:28:50 -0400

    Unit Tests

--- tests/Grimware.Common.UnitTests.Shared/Extensions/EnumerableExtensionsTests.cs
+++ tests/Grimware.Common.UnitTests.Shared/Extensions/EnumerableExtensionsTests.cs
@@ -6,6 +6,8 @@ using FluentAssertions;
 using Grimware.Extensions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NSubstitute;
+// ReSharper disable JoinDeclarationAndInitializer
+// ReSharper disable RedundantAssignment
 
 // ReSharper disable ReturnValueOfPureMethodIsNotUsed
 // ReSharper disable PossibleMultipleEnumeration
@@ -52,6 +54,359 @@ namespace Grimware.Common.UnitTests.Extensions
             _Int32TestData.Concat(101).Should().BeEquivalentTo(new[] { Int32.MinValue, -1, 0, 1, Int32.MaxValue, 101 });
 
             // Assert
+        }
+
+        [TestMethod]
+        public void Convert()
+        {
+            // Arrange
+            IEnumerable<long> results;
+
+            // Act
+            results = _Int32TestData.Convert(i => (long)i);
+
+            // Assert
+            results.Should().NotBeNull();
+            results.Should().BeEquivalentTo(_Int32TestData.Select(i => (long)i));
+        }
+
+        [TestMethod]
+        public void Convert_Exceptions()
+        {
+            // Arrange
+            IEnumerable<long> results = null;
+
+            // Act
+            Action act = () => results = _testEnumerable.Convert<int, long>(null);
+
+            // Assert
+            act.Should().Throw<ArgumentNullException>();
+            results.Should().BeNull();
+        }
+
+        [TestMethod]
+        public void Distinct_BySelectors()
+        {
+            // Arrange
+            IEnumerable<int> results;
+
+            // Act
+            results = _Int32TestData.Distinct((i, j) => i == j, i => i.GetHashCode());
+
+            // Assert
+            ((IEnumerable<int>)null).Distinct((i, j) => i == j, i => i.GetHashCode()).Should().BeNull();
+
+            results.Should().NotBeNull();
+            results.Should().NotBeSameAs(_Int32TestData);
+            results.Should().BeEquivalentTo(_Int32TestData);
+        }
+
+        [TestMethod]
+        public void Distinct_ByKey()
+        {
+            // Arrange
+            IEnumerable<int> results;
+
+            // Act
+            results = _Int32TestData.Distinct(i => i);
+
+            // Assert
+            ((IEnumerable<int>)null).Distinct(i => i).Should().BeNull();
+
+            results.Should().NotBeNull();
+            results.Should().NotBeSameAs(_Int32TestData);
+            results.Should().BeEquivalentTo(_Int32TestData);
+        }
+
+        [TestMethod]
+        public void Distinct_Exceptions()
+        {
+            // Arrange
+            IEnumerable<int> results = null;
+
+            // Act
+            Action act1 = () => results = _testEnumerable.Distinct(null, i => i.GetHashCode());
+            Action act2 = () => results = _testEnumerable.Distinct((i, j) => i == j, null);
+            Action act3 = () => results = _testEnumerable.Distinct((Func<int, int>)null);
+
+            // Assert
+            act1.Should().Throw<ArgumentNullException>().Where(ex => "equalityComparison".Equals(ex.ParamName));
+            results.Should().BeNull();
+
+            act2.Should().Throw<ArgumentNullException>().Where(ex => "hashGenerator".Equals(ex.ParamName));
+            results.Should().BeNull();
+
+            act3.Should().Throw<ArgumentNullException>().Where(ex => "keySelector".Equals(ex.ParamName));
+            results.Should().BeNull();
+        }
+
+        [TestMethod]
+        public void Except()
+        {
+            // Arrange
+            IEnumerable<int> results;
+
+            // Act
+            results = _Int32TestData.Except(0);
+
+            // Assert
+            results.Should().NotBeNull();
+            results.Should().BeEquivalentTo(new[] { Int32.MinValue, -1, 1, Int32.MaxValue });
+        }
+
+        [TestMethod]
+        public void FirstIndexWhere()
+        {
+            // Arrange
+
+            // Act
+            ((IEnumerable<int>)null).FirstIndexWhere(i => i == 0).Should().BeNull();
+            _Int32TestData.FirstIndexWhere(i => i == 101).Should().BeNull();
+
+            _Int32TestData.FirstIndexWhere(i => i == Int32.MinValue).Should().Be(0);
+            _Int32TestData.FirstIndexWhere(i => i == -1).Should().Be(1);
+            _Int32TestData.FirstIndexWhere(i => i == 0).Should().Be(2);
+            _Int32TestData.FirstIndexWhere(i => i == 1).Should().Be(3);
+            _Int32TestData.FirstIndexWhere(i => i == Int32.MaxValue).Should().Be(4);
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void ForEach()
+        {
+            // Arrange
+            var list = new List<int>(8);
+
+            // Act
+            ((IEnumerable<int>)null).ForEach(i => i++);
+            _testEnumerable.ForEach(null);
+
+            _Int32TestData.ForEach(i => list.Add(i));
+
+            // Assert
+            list.Should().BeEquivalentTo(_Int32TestData);
+        }
+
+        [TestMethod]
+        public void Last()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            _Int32TestData.Last(2).Should().BeEquivalentTo(new[] { 1, Int32.MaxValue });
+            _Int32TestData.Last(i => i <= 0, 2).Should().BeEquivalentTo(new[] { -1, 0 });
+        }
+
+        [TestMethod]
+        public void None()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            ((IEnumerable<int>)null).None().Should().BeTrue();
+            ((IEnumerable<int>)null).None().Should().BeTrue();
+
+            _Int32TestData.None().Should().BeFalse();
+            _Int32TestData.Where(i => i == 101).None().Should().BeTrue();
+
+            _Int32TestData.None(i => i == 0).Should().BeFalse();
+            _Int32TestData.None(i => i == 101).Should().BeTrue();
+        }
+
+        private static readonly IEnumerable<TestClass> _TestClassesBefore =
+            new[]
+            {
+                new TestClass { Data = 0 },
+                new TestClass { Data = 1 },
+                new TestClass { Data = 2 },
+                new TestClass { Data = 3 },
+                new TestClass { Data = 4 },
+                new TestClass { Data = 5 },
+                new TestClass { Data = 6 },
+                new TestClass { Data = 7 },
+                new TestClass { Data = 8 },
+                new TestClass { Data = 9 },
+            };
+        private static readonly IEnumerable<TestClass> _TestClassesAfter =
+            _TestClassesBefore
+                    // Make sure we're modifying copies, not the originals
+                   .Select(x => x.Clone())
+                    // Add some
+                   .Concat(
+                        new[]
+                        {
+                            new TestClass { Data = 101 },
+                            new TestClass { Data = 102 },
+                            new TestClass { Data = 103 },
+                            new TestClass { Data = 104 },
+                            new TestClass { Data = 105 },
+                        })
+                    // Modify some
+                   .Select(
+                        x =>
+                        {
+                            if (x.Data % 2 != 0)
+                                x.Data += 10;
+
+                            return x;
+                        })
+                    // Remove some
+                   .Where(x => x.Data % 3 != 0)
+                    // Enumerate
+                   .ToArray();
+
+
+        [TestMethod]
+        public void Diff()
+        {
+            // Arrange
+
+            // Act
+            var diff = _TestClassesBefore.Diff(_TestClassesAfter, x => x.Key, x => x.Data);
+
+            // Assert
+            diff.Should().NotBeNull();
+
+            var added = diff.Added;
+            added.Should().NotBeNull();
+            added.Count().Should().Be(3);
+            added.Select(x => x.Data).Should().BeEquivalentTo(new[] { 113, 104, 115 });
+
+            var removed = diff.Removed;
+            removed.Should().NotBeNull();
+            removed.Count().Should().Be(3);
+            removed.Select(x => x.Data).Should().BeEquivalentTo(new[] { 0, 5, 6 });
+
+            var modified = diff.Modified;
+            modified.Should().NotBeNull();
+            modified.Count().Should().Be(4);
+            modified.Select(x => x.Data).Should().BeEquivalentTo(new[] { 11, 13, 17, 19 });
+
+            var unmodified = diff.Unmodified;
+            unmodified.Should().NotBeNull();
+            unmodified.Count().Should().Be(3);
+            unmodified.Select(x => x.Data).Should().BeEquivalentTo(new[] { 2, 4, 8 });
+        }
+
+        [TestMethod]
+        public void Diff_Exceptions()
+        {
+            // Arrange
+            IDiffResult<TestClass> diff = null;
+
+            Action act1 = () => diff = _TestClassesBefore.Diff<TestClass, Guid, int>(_TestClassesAfter, null, x => x.Data);
+            Action act2 = () => diff = _TestClassesBefore.Diff<TestClass, Guid, int>(_TestClassesAfter, x => x.Key, null);
+
+            // Act
+
+            // Assert
+            act1.Should().Throw<ArgumentNullException>().Where(ex => "keySelector".Equals(ex.ParamName));
+            act2.Should().Throw<ArgumentNullException>().Where(ex => "dataSelector".Equals(ex.ParamName));
+        }
+
+        [TestMethod]
+        public void SelectAdded()
+        {
+            // Arrange
+
+            // Act
+            var added = _TestClassesBefore.SelectAdded(_TestClassesAfter, x => x.Key);
+
+            // Assert
+            added.Should().NotBeNull();
+            added.Count().Should().Be(3);
+            added.Select(x => x.Data).Should().BeEquivalentTo(new[] { 113, 104, 115 });
+        }
+
+        [TestMethod]
+        public void SelectedAdded_Exceptions()
+        {
+            // Arrange
+            IEnumerable<TestClass> added = null;
+
+            // Act
+            Action act = () => added = _TestClassesBefore.SelectAdded<TestClass, Guid>(_TestClassesAfter, null);
+
+            // Assert
+            act.Should().Throw<ArgumentNullException>().Where(ex => "keySelector".Equals(ex.ParamName));
+            added.Should().BeNull();
+        }
+
+        [TestMethod]
+        public void SelectModified()
+        {
+            // Arrange
+
+            // Act
+            var modified = _TestClassesBefore.SelectModified(_TestClassesAfter, x => x.Key, x => x.Data);
+
+            // Assert
+            modified.Should().NotBeNull();
+            modified.Count().Should().Be(4);
+            modified.Select(x => x.Data).Should().BeEquivalentTo(new[] { 11, 13, 17, 19 });
+        }
+
+        [TestMethod]
+        public void SelectModified_Exceptions()
+        {
+            // Arrange
+            IEnumerable<TestClass> modified = null;
+
+            // Act
+            Action act1 = () => modified = _TestClassesBefore.SelectModified<TestClass, Guid, int>(_TestClassesAfter, null, x => x.Data);
+            Action act2 = () => modified = _TestClassesBefore.SelectModified<TestClass, Guid, int>(_TestClassesAfter, x => x.Key, null);
+
+            // Arrange
+            act1.Should().Throw<ArgumentNullException>().Where(ex => "keySelector".Equals(ex.ParamName));
+            modified.Should().BeNull();
+
+            act2.Should().Throw<ArgumentNullException>().Where(ex => "dataSelector".Equals(ex.ParamName));
+            modified.Should().BeNull();
+        }
+
+        [TestMethod]
+        public void SelectRemoved()
+        {
+            // Arrange
+
+            // Act
+            var removed = _TestClassesBefore.SelectRemoved(_TestClassesAfter, x => x.Key);
+
+            // Assert
+            removed.Should().NotBeNull();
+            removed.Count().Should().Be(3);
+            removed.Select(x => x.Data).Should().BeEquivalentTo(new[] { 0, 5, 6 });
+        }
+
+        [TestMethod]
+        public void SelectRemoved_Exceptions()
+        {
+            // Arrange
+            IEnumerable<TestClass> removed = null;
+
+            // Act
+            Action act = () => removed = _TestClassesBefore.SelectRemoved<TestClass, Guid>(_TestClassesAfter, null);
+
+            // Assert
+            act.Should().Throw<ArgumentNullException>().Where(ex => "keySelector".Equals(ex.ParamName));
+            removed.Should().BeNull();
+        }
+
+        private class TestClass
+        {
+            public Guid Key { get; private set; } = Guid.NewGuid();
+
+            public int Data { get; set; }
+
+            public TestClass Clone()
+            {
+                return new TestClass { Key = this.Key, Data = this.Data };
+            }
         }
     }
 }
--- tests/Grimware.Common.UnitTests.Shared/Extensions/StringExtensionsTests.cs
+++ tests/Grimware.Common.UnitTests.Shared/Extensions/StringExtensionsTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using FluentAssertions;
 using Grimware.Extensions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -9,6 +10,141 @@ namespace Grimware.Common.UnitTests.Extensions
     [ExcludeFromCodeCoverage]
     public class StringExtensionsTests
     {
+        private const string _NullString = ((string)null);
+
+        [TestMethod]
+        public void ConvertTo()
+        {
+            var testDate = DateTime.Now;
+            var testTime = DateTime.Now - DateTime.Today;
+
+            "true".ConvertTo(false).Should().Be(true);
+            "-1".ConvertTo(false).Should().Be(false);
+            "1999-12-31".ConvertTo(false).Should().Be(false);
+            "23:59:59.999".ConvertTo(false).Should().Be(false);
+
+            "true".ConvertTo(0).Should().Be(0);
+            "-1".ConvertTo(0).Should().Be(-1);
+            "1999-12-31".ConvertTo(0).Should().Be(0);
+            "23:59:59.999".ConvertTo(0).Should().Be(0);
+
+            "true".ConvertTo(testDate).Should().Be(testDate);
+            "-1".ConvertTo(testDate).Should().Be(testDate);
+            "1999-12-31".ConvertTo(testDate).Should().Be(new DateTime(1999, 12, 31));
+            "23:59:59.999".ConvertTo(testDate).Should().Be(DateTime.Parse("23:59:59.999"));
+
+            "true".ConvertTo(testTime).Should().Be(testTime);
+            "-1".ConvertTo(testTime).Should().Be(TimeSpan.Parse("-1"));
+            "1999-12-31".ConvertTo(testTime).Should().Be(testTime);
+            "23:59:59.999".ConvertTo(testTime).Should().Be(TimeSpan.Parse("23:59:59.999"));
+        }
+
+        [TestMethod]
+        public void In()
+        {
+            _NullString.In("Lorem", "ipsum", "dolor", "sit", "amet", null).Should().BeTrue();
+
+            _NullString.In("Lorem", "ipsum", "dolor", "sit", "amet").Should().BeFalse();
+            "".In("Lorem", "ipsum", "dolor", "sit", "amet").Should().BeFalse();
+            "Lorem".In("Lorem", "ipsum", "dolor", "sit", "amet").Should().BeTrue();
+            "Ipsum".In("Lorem", "ipsum", "dolor", "sit", "amet").Should().BeFalse();
+            "dolor".In("Lorem", "ipsum", "dolor", "sit", "amet").Should().BeTrue();
+
+            _NullString.In(StringComparison.OrdinalIgnoreCase, "Lorem", "ipsum", "dolor", "sit", "amet").Should().BeFalse();
+            "".In(StringComparison.OrdinalIgnoreCase, "Lorem", "ipsum", "dolor", "sit", "amet").Should().BeFalse();
+            "Lorem".In(StringComparison.OrdinalIgnoreCase, "Lorem", "ipsum", "dolor", "sit", "amet").Should().BeTrue();
+            "Ipsum".In(StringComparison.OrdinalIgnoreCase, "Lorem", "ipsum", "dolor", "sit", "amet").Should().BeTrue();
+            "dolor".In(StringComparison.OrdinalIgnoreCase, "Lorem", "ipsum", "dolor", "sit", "amet").Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void NullIf()
+        {
+            _NullString.NullIf(true, null).Should().BeNull();
+            "".NullIf(true, "").Should().BeNull();
+            "".NullIf(true, null).Should().Be("");
+            "Test".NullIf(true, "test").Should().BeNull();
+            "Test".NullIf(false, "test").Should().Be("Test");
+        }
+
+        [TestMethod]
+        public void NullIfEmpty()
+        {
+            _NullString.NullIfEmpty().Should().BeNull();
+            "".NullIfEmpty().Should().BeNull();
+            " ".NullIfEmpty().Should().Be(" ");
+            "Test".NullIfEmpty().Should().Be("Test");
+        }
+
+        [TestMethod]
+        public void NullIfIn()
+        {
+            _NullString.NullIfIn(false, "Lorem", "ipsum", "dolor", "sit", "amet", null).Should().BeNull();
+
+            _NullString.NullIfIn(false, "Lorem", "ipsum", "dolor", "sit", "amet").Should().BeNull();
+            "".NullIfIn(false, "Lorem", "ipsum", "dolor", "sit", "amet").Should().NotBeNull();
+            "Lorem".NullIfIn(false, "Lorem", "ipsum", "dolor", "sit", "amet").Should().BeNull();
+            "Ipsum".NullIfIn(false, "Lorem", "ipsum", "dolor", "sit", "amet").Should().NotBeNull();
+            "dolor".NullIfIn(false, "Lorem", "ipsum", "dolor", "sit", "amet").Should().BeNull();
+
+            _NullString.NullIfIn(true, "Lorem", "ipsum", "dolor", "sit", "amet").Should().BeNull();
+            "".NullIfIn(true, "Lorem", "ipsum", "dolor", "sit", "amet").Should().NotBeNull();
+            "Lorem".NullIfIn(true, "Lorem", "ipsum", "dolor", "sit", "amet").Should().BeNull();
+            "Ipsum".NullIfIn(true, "Lorem", "ipsum", "dolor", "sit", "amet").Should().BeNull();
+            "dolor".NullIfIn(true, "Lorem", "ipsum", "dolor", "sit", "amet").Should().BeNull();
+        }
+
+        [TestMethod]
+        public void NullIfWhitespace()
+        {
+            _NullString.NullIfWhitespace().Should().BeNull();
+            "".NullIfWhitespace().Should().BeNull();
+            " ".NullIfWhitespace().Should().BeNull();
+            "Test".NullIfWhitespace().Should().Be("Test");
+        }
+
+        [TestMethod]
+        public void Split()
+        {
+            _NullString.Split(" ").Should().BeEquivalentTo(Array.Empty<string>());
+
+            "".Split(" ").Should().BeEquivalentTo("");
+            "".Split(" ", StringSplitOptions.RemoveEmptyEntries).Should().BeEquivalentTo(Array.Empty<string>());
+
+            "Lorem ipsum dolor sit amet".Split(" ").Should().BeEquivalentTo("Lorem", "ipsum", "dolor", "sit", "amet");
+        }
+
+        [TestMethod]
+        public void StripNonAlphaCharacters()
+        {
+            "Bazinga!".StripNonAlphaCharacters().Should().Be("Bazinga");
+            "B a z i n g a !™ 1.2.3.".StripNonAlphaCharacters().Should().Be("Bazinga");
+        }
+
+        [TestMethod]
+        public void StripNonAlphanumericCharacters()
+        {
+            "Bazinga!".StripNonAlphanumericCharacters().Should().Be("Bazinga");
+            "B a z i n g a !™ 1.2.3.".StripNonAlphanumericCharacters().Should().Be("Bazinga123");
+
+        }
+
+        [TestMethod]
+        public void StripNonAlphanumericOrWhiteSpaceCharacters()
+        {
+            "Bazinga!".StripNonAlphanumericOrWhiteSpaceCharacters().Should().Be("Bazinga");
+            "B a z i n g a !™ 1.2.3.".StripNonAlphanumericOrWhiteSpaceCharacters().Should().Be("B a z i n g a  123");
+            " \t\r\n 1.2.3.".StripNonAlphanumericOrWhiteSpaceCharacters().Should().Be(" \t\r\n 123");
+        }
+
+        [TestMethod]
+        public void ToDateTime()
+        {
+
+        }
+
+
+
         [DataTestMethod]
         [DataRow(null)]
         [DataRow("2019 © Assurant, Inc.")]

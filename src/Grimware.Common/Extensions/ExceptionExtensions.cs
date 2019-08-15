465108f175c97b0d328d9aeec8b8c9d2c822b75e
Toby Herring <30664204+GrimGadget@users.noreply.github.com>
2019-08-15 17:06:31 -0400

    Set up CI with Azure Pipelines (#7)
    * Set up CI with Azure Pipelines
    
    [skip ci]
    
    * Added -alpha to version base
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Tweak ExceptionExtensions
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Version change to make compatible with SemVer 1.0
    
    * Include *.UnitTests.*.dll in test run

--- src/Grimware.Common/Extensions/ExceptionExtensions.cs
+++ src/Grimware.Common/Extensions/ExceptionExtensions.cs
@@ -8,8 +8,8 @@ namespace Grimware.Extensions
     {
         private const string _AppIdKey = ".appId";
         private const string _EndOfInnerExceptionStackTrace = " End of InnerException Stack Trace";
-        private const string _VerboseExceptionFormat = "{1}: {2}{0}{3}{4}";
-        private const string _VerboseInnerExceptionFormat = "{0} ---> {1}{0}   {2}{0}{0}";
+        private const string _VerboseExceptionFormat = "{1}: {2}{0}{3}{4}{0}";
+        private const string _VerboseInnerExceptionFormat = " ---> {1}   {2}{0}{0}";
 
         public static string ToStringVerbose(this Exception exception)
         {
